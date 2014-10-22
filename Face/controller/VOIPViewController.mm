05cd9fce5365a0547f603e9cd5cf20b255e8c2cc
杨朋亮 <yangpengliang@yangpengliangdeMacBook-Pro.local>
2014-10-23 00:16:48 +0800

    UI修改2

--- Face/controller/VOIPViewController.mm
+++ Face/controller/VOIPViewController.mm
@@ -64,6 +64,9 @@
 @property(nonatomic) UIButton *hangUpButton;
 @property(nonatomic) UIButton *acceptButton;
 @property(nonatomic) UIButton *refuseButton;
+@property (nonatomic) UIButton *changeStateButton;
+
+@property(nonatomic) BOOL isLoudspeaker;
 
 @property(nonatomic) History *history;
 @end
@@ -126,11 +129,22 @@
     }
     
     [[IMService instance] pushVOIPObserver:self];
+    
+    [self.view setBackgroundColor:[UIColor blackColor]];
+    
+    
+    UIImageView *headView = [[UIImageView alloc] initWithFrame:CGRectMake((self.view.frame.size.width - 120)/2, 80, 120, 120)];
+    [headView sd_setImageWithURL:[[NSURL alloc] initWithString:self.peerUser.avatarURL] placeholderImage:[UIImage imageNamed:@"manHeader"]];
+    CALayer *imageLayer = [headView layer];   //获取ImageView的层
+    [imageLayer setMasksToBounds:YES];
+    [imageLayer setCornerRadius:headView.frame.size.width/2];
+    [self.view addSubview: headView];
 
     UIButton *moreButton = [UIButton buttonWithType:UIButtonTypeCustom];
     moreButton.backgroundColor = [UIColor colorWithRed:0.78f green:0.78f blue:0.8f alpha:1.0f];
-    moreButton.frame = CGRectMake(30.0f, 50.0f, 100, 50);
+    moreButton.frame = CGRectMake(30.0f, self.view.frame.size.height - 50.0f - 50.0f , 100, 50);
     [moreButton setTitle:@"挂断" forState:UIControlStateNormal];
+    [moreButton  setBackgroundColor:[UIColor redColor]];
     [moreButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
     [moreButton addTarget:self action:@selector(hangUp:) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:moreButton];
@@ -138,8 +152,9 @@
     
     moreButton = [UIButton buttonWithType:UIButtonTypeCustom];
     moreButton.backgroundColor = [UIColor colorWithRed:0.78f green:0.78f blue:0.8f alpha:1.0f];
-    moreButton.frame = CGRectMake(30.0f, 50.0f, 100, 50);
+    moreButton.frame = CGRectMake(30.0f, self.view.frame.size.height - 50.0f - 50.0f, 100, 50);
     [moreButton setTitle:@"接听" forState:UIControlStateNormal];
+    [moreButton setBackgroundColor:[UIColor greenColor]];
     [moreButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
     [moreButton addTarget:self action:@selector(acceptCall:) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:moreButton];
@@ -147,12 +162,21 @@
     
     moreButton = [UIButton buttonWithType:UIButtonTypeCustom];
     moreButton.backgroundColor = [UIColor colorWithRed:0.78f green:0.78f blue:0.8f alpha:1.0f];
-    moreButton.frame = CGRectMake(150.0f, 50.0f, 100, 50);
+    moreButton.frame = CGRectMake(150.0f, self.view.frame.size.height - 50.0f - 50.0f, 100, 50);
     [moreButton setTitle:@"拒绝" forState:UIControlStateNormal];
+    [moreButton setBackgroundColor:[UIColor redColor]];
     [moreButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
     [moreButton addTarget:self action:@selector(refuseCall:) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:moreButton];
     self.refuseButton = moreButton;
+    
+    CGRect frame = CGRectMake(240, self.view.frame.size.height - 50.0f - 50.0f, 60, 60);
+    self.changeStateButton = [[UIButton alloc] initWithFrame:frame];
+    
+    [self.changeStateButton setBackgroundImage:[UIImage imageNamed:@"loader.png"] forState:UIControlStateNormal];
+    [self.changeStateButton addTarget:self action:@selector(changelistenState:) forControlEvents:UIControlEventTouchUpInside];
+    
+    [self.view addSubview:self.changeStateButton];
     
     if (self.isCaller) {
         self.acceptButton.hidden = YES;
@@ -242,6 +266,17 @@
         [self dismiss];
     } else {
         NSLog(@"invalid voip state:%d", voip.state);
+    }
+}
+
+-(void) changelistenState:(id)sender{
+    self.isLoudspeaker = !self.isLoudspeaker;
+    if (self.isLoudspeaker) {
+           [self.changeStateButton setBackgroundImage:[UIImage imageNamed:@"phone.png"] forState:UIControlStateNormal];
+        self.recvStream.isLoudspeaker = YES;
+    }else{
+          [self.changeStateButton setBackgroundImage:[UIImage imageNamed:@"loader.png"] forState:UIControlStateNormal];
+        self.recvStream.isLoudspeaker = NO;
     }
 }
 
