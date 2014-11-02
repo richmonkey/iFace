a0ed0ed649872948eaa256e4937a1268e3b9bf3b
杨朋亮 <yangpengliang@yangpengliangdeMacBook-Pro.local>
2014-11-02 16:17:54 +0800

    资源更换

---
+++ Face/controller/SettingViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingViewController.h
+//  Face
+//
+//  Created by 杨朋亮 on 2/11/14.
+//  Copyright (c) 2014年 beetle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingViewController : UIViewController
+
+@end
