83f3a1f79776a6d697fe7861d3e7b33b2e64be6d
杨朋亮 <yangpengliang@yangpengliangdeMacBook-Pro.local>
2014-11-02 21:59:52 +0800

    资源更改和UI修改

---
+++ Face/cell/HistoryTableViewCell.h
@@ -0,0 +1,17 @@
+//
+//  HistoryTableViewCell.h
+//  Face
+//
+//  Created by 杨朋亮 on 2/11/14.
+//  Copyright (c) 2014年 beetle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HistoryTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *durationLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *iconView;
+@property (weak, nonatomic) IBOutlet UILabel *statusLabel;
+
+@end
---
+++ Face/cell/HistoryTableViewCell.m
@@ -0,0 +1,25 @@
+//
+//  HistoryTableViewCell.m
+//  Face
+//
+//  Created by 杨朋亮 on 2/11/14.
+//  Copyright (c) 2014年 beetle. All rights reserved.
+//
+
+#import "HistoryTableViewCell.h"
+
+@implementation HistoryTableViewCell
+
+- (void)awakeFromNib
+{
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
